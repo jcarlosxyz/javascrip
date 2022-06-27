de7a148fabd7a624d9c52066eeb79c0d48d42609
jcarlosxyz <jcarlosxyz@gmail.com>
2022-06-27 17:56:51 -0500

    Nuevo linux

---
+++ fundamentos/js/app.js
@@ -0,0 +1,12 @@
+//console.log("hola mundo ")
+alert("Hola---------")
+let nombre = prompt("Cual es tu nombre");
+let desicion =  false
+desicion = confirm("Esta seguro...........<<");
+if (desicion == true){
+      console.log("Es verdadero");
+}else
+  { 
+     console.log("Es falso")   
+}
+//console.log(desicion);
